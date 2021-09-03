079da7c3b40e1bd4fbb8a6144d7b6e7e8daa0725
alan <alanjosedossantos011@gmail.com>
2021-09-03 17:55:49 -0300

    entities settings

---
+++ src/entities/Settings.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+@Entity('settings')
+export default class Settings {
+    
+    @PrimaryColumn({})
+    id: string;
+
+    @Column()
+    username: string;
+    chat: boolean;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+
+}
